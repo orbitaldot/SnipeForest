748b1066f2fbb40e96c48e57e548829d141d1ca5
orbitaldot <orbitaldotogg@gmail.com>
2023-12-17 19:53:41 +0100

    Update Crosshair.gd

--- Code/Crosshair.gd
+++ Code/Crosshair.gd
@@ -3,14 +3,11 @@ extends Node2D
 var sprite:Sprite2D
 var rotate_speed:float = 0.005;
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	self.sprite = get_node('Sprite2D')
 	self.sprite.scale = Vector2(10.0,10.0)
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.sprite.scale.x = lerp(self.sprite.scale.x, 0.75, 0.1)
 	self.sprite.scale.y = lerp(self.sprite.scale.y, 0.75, 0.1)
